7d8b14ca6ce85f36a22186357150a5de47fe5e77
Luis Malta <luiso.malta@gmail.com>
2023-07-22 13:54:33 -0300

    [IMP] city_revenue: consult last revenue date

--- minas_data_lab/assets/city_revenue/city_revenue.py
+++ minas_data_lab/assets/city_revenue/city_revenue.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from io import StringIO
+from datetime import datetime
 from dagster import Output, asset, MetadataValue
 
 from minas_data_lab.resources.PortalTransparenciaScrapper import PortalTransparenciaScrapper
+
+SELECT_LAST_REVENUE_DATE = 'select date from revenue order by date desc limit 1'
 
 
 @asset(
@@ -10,7 +13,8 @@ from minas_data_lab.resources.PortalTransparenciaScrapper import PortalTranspare
     key_prefix=["revenue"],
 )
 def city_revenue(context) -> pd.DataFrame:
-    payload = ['INT_EXR=2023', 'CHAR_ID_EMP=1', 'LG_ALT_PAG=S', 'URL=Tempo_Real_Receitas']
+    month = datetime.now().month - 1
+    payload = ['INT_EXR=2023', 'CHAR_ID_EMP=1', 'LG_ALT_PAG=S', f'INT_MES_INI={month}', f'INT_MES_FIM={month}', 'URL=Tempo_Real_Receitas']
     scrapper = PortalTransparenciaScrapper('saojoaodelrei')
     report = scrapper.get_report('Tempo_Real_Receitas', 'csv', payload)
 
@@ -20,9 +24,10 @@ def city_revenue(context) -> pd.DataFrame:
 
 @asset(
     io_manager_key="postgres_io_manager",
+    required_resource_keys={"postgres_resource"},
     key_prefix=["revenue"],
 )
-def city_revenue_cleaned(city_revenue: pd.DataFrame) -> Output[pd.DataFrame]:
+def city_revenue_cleaned(context, city_revenue: pd.DataFrame) -> Output[pd.DataFrame]:
     columns_rename = {
         'Data': 'date',
         'Tipo de Minuta': 'draft_type',
@@ -39,6 +44,10 @@ def city_revenue_cleaned(city_revenue: pd.DataFrame) -> Output[pd.DataFrame]:
     city_revenue.rename(columns=columns_rename, inplace=True)
 
     city_revenue['date'] = pd.to_datetime(city_revenue['date'], format="%d/%m/%Y")
+
+    query_cursor = context.resources.postgres_resource.execute_query(SELECT_LAST_REVENUE_DATE)
+    last_revenue_date = query_cursor.one()[0]
+    city_revenue.query(f'date > "{last_revenue_date}"', inplace=True)
 
     return Output(
         city_revenue,
